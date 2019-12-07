9b05d67f99b5747b0f45e5acc0326abb5547a5bf
maincorename <turbocatrom@gmail.com>
2019-12-07 23:01:44 +0700

    готовый отлов погоды

--- src/main/java/weather.java
+++ src/main/java/weather.java
@@ -5,25 +5,12 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class weather {
 
     private static Document getPage() throws IOException {
         String url = "http://www.m.pogodavtomske.ru";
-        Document page = Jsoup.parse(new URL(url), 3000);
-        return page;
-    }
-
-    private static Pattern pattern = Pattern.compile("\\d{1}");
-
-    private static String searchDate(String stringDate) throws Exception {
-        Matcher matcher = pattern.matcher(stringDate);
-        if(matcher.find()){
-            return matcher.group();
-        }
-        throw new Exception("Не удалось извлечь дату из строки");
+        return Jsoup.parse(new URL(url), 3000);
     }
 
     public static void main(String[] args) throws Exception {
@@ -32,12 +19,18 @@ public class weather {
         Element tableWthr = page.select("table[class=whdisplay]").first(); //таблица погоды
         Elements values = tableWthr.select("tr[height=125px]"); //строка погоды
 
-        for (Element name : values ) {
-            String dateString = name.select("").toString();
-            String date = searchDate(dateString);
-            System.out.println("дата    осадки    температура    ветер    давление");
-        }
+        int index = 0;
+        printWeatherValues(values, index);
     }
 
-
+    private static void printWeatherValues(Elements values, int index){
+        for (int i = 0; i < 4; i++){
+            Element valueLine = values.get(index);
+            for(Element td : valueLine.select("td")){ //вывод погоды на день
+                System.out.print(td.text() + "    ");
+            }
+            System.out.println();
+            index++; //переход на следующий день
+        }
+    }
 }
